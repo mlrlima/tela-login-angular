11f1e588c7843d1e1e75d7204763382c36c50778
Maria Luiza Lima <mlrlima04@gmail.com>
2026-07-13 16:39:48 -0300

    refactor: frontend no tomcat

---
+++ src/main/java/controller/SpaFallbackController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaFallbackController {
+
+    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
