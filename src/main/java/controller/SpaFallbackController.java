696a9afcc715c4083ad586d83048071162d89edb
Maria Luiza Lima <mlrlima04@gmail.com>
2026-07-13 17:00:08 -0300

    refactor: tentando fazer a api com tomcat

--- src/main/java/controller/SpaFallbackController.java
+++ src/main/java/controller/SpaFallbackController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaFallbackController {
 
-    @RequestMapping(value = "/**/{path:[^\\.]*}")
-    public String forward() {
-        return "forward:/index.html";
-    }
+	@RequestMapping(value = {
+	        "/{path:^(?!api|assets|.*\\.).*$}",
+	        "/{path:^(?!api|assets|.*\\.).*$}/**"
+	    })
+	    public String forward() {
+	        return "forward:/index.html";
+	    }
 }
