10166a57f1378fe8aea957a15da5240ba3878e76
Maria Luiza Lima <mlrlima04@gmail.com>
2026-07-29 16:47:30 -0300

    feat: testando websocket

---
+++ angular-frontend/src/app/services/websocket-service.ts
@@ -0,0 +1,39 @@
+import { Service } from '@angular/core';
+import {CompatClient, Stomp} from '@stomp/stompjs';
+import {StompSubscription} from '@stomp/stompjs/src/stomp-subscription';
+
+export type ListenerCallBack = (message: String) => void;
+
+@Injectable({ providedIn:'root'})
+export class WebsocketService implements OnDestroy {
+	
+	private connection: CompatClient | undefined = undefined;
+	
+	private subscription: StompSubscription | undefined;
+	
+	constructor() {
+	  this.connection = Stomp.client(`${environment.apiUrl}/websocket`);
+	  this.connection.connect({}, () => {});
+	}
+	
+	public send(str: String): void {
+	  if (this.connection && this.connection.connected) {
+	    this.connection.send(`/teste-dois/add_string`, {}, "hello world!!!!!!!!!!");
+	  }
+	}
+	
+	public listen(fun: ListenerCallBack): void {
+	  if (this.connection) {
+	    this.connection.connect({}, () => {
+	      this.subscription = this.connection!.subscribe(`/teste/added_string`, message => fun(JSON.parse(message.body)));
+	    }); 
+	  }
+	}
+	
+	ngOnDestroy(): void {
+	  if (this.subscription) {
+	    this.subscription.unsubscribe();
+	  }
+	}
+	
+}
---
+++ angular-frontend/src/app/teste-websocket/teste-websocket.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WebsocketService } from '../services/websocket.service';
+
+@Component({
+  selector: 'app-teste-websocket',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './teste-websocket.html',
+  styleUrl: './teste-websocket.css',
+}) 
+export class TesteWebsocket {
+	mensagens: string[] = [];
+
+	constructor(private websocketService: WebsocketService,) {}
+
+	conectar() {
+	  this.websocketService.listen((msg: String) => {
+	    console.log('Mensagem recebida:', msg);
+	    this.mensagens.push(msg.toString());
+	  });
+	}
+
+	enviar() {
+	  this.websocketService.send("hello world");
+	}
+}
+
+
